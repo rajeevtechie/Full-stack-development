79542514e0d1954fecbf2044b33a8764c8a1ee15
rajeevtechie <rajeev.techie380@gmail.com>
2026-02-20 10:48:56 +0530

    learned handling get and post request

--- full_stack/backend/index.js
+++ full_stack/backend/index.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get("/register",(req,response)=>{
     let {user,password} = req.query;
     response.send(`Standard get response. Welcome, ${user}`);
 });
 app.post("/register",(req,response)=>{
-    response.send("standard post response");
+    let {user,password} = req.body;
+    response.send(`standard post response. Welcome, ${user}`);
 });
 
 
